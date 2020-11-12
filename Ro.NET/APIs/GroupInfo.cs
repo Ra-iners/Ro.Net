63806bd34b3bcfd53011635f1ef8e58c5bab8b5d
rainers <35530071+rainersyt@users.noreply.github.com>
2020-11-12 23:08:57 +0200

    Initial commit

---
+++ Ro.NET/APIs/GroupInfo.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ro.NET
+{
+    public class RobloxGroup : IDisposable
+    {
+        readonly WebClient web;
+
+        public string Name;
+        public ulong Id;
+        public string OwnerUsername;
+        public ulong OwnerId;
+        public string EmblemUrl;
+        public string Description;
+        public Dictionary<int, string> Roles = new Dictionary<int, string>();
+
+        public RobloxGroup(ulong GroupID)
+        {
+            web = new WebClient();
+            string RAW_JSON;
+            try
+            {
+                RAW_JSON = web.DownloadString($"http://api.roblox.com/groups/{GroupID}");
+            }
+            catch
+            {
+                RAW_JSON = "Error";
+                return;
+            }
+            if(RAW_JSON == "Error")
+            {
+                Name = "N/A";
+                Id = 0;
+                OwnerUsername = "N/A";
+                OwnerId = 0;
+                EmblemUrl = "N/A";
+                Description = "N/A";
+                return;
+            }
+            dynamic JSON = JObject.Parse(RAW_JSON);
+            Name = JSON.Name;
+            Id = JSON.Id;
+            OwnerUsername = JSON.Owner.Name;
+            OwnerId = JSON.Owner.Id;
+            EmblemUrl = JSON.EmblemUrl;
+            Description = JSON.Description;
+
+            foreach(var Role in JSON.Roles)
+            {
+                string RankName = Role.Name;
+                int RankID = Role.Rank;
+                if (!Roles.ContainsKey(RankID))
+                {
+                    Roles.Add(RankID, RankName);
+                }
+                else
+                {
+                    Debug.Write($"key already exists {RankID}; skipping...");
+                }
+            }
+
+        }
+
+        public void Dispose()
+        {
+            web.Dispose();
+        }
+    }
+}
---
+++ Ro.NET/APIs/UserInfo.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ro.NET
+{
+    public class RobloxUser : IDisposable
+    {
+        WebClient webClient;
+
+        public string Username;
+        public ulong Id;
+        public bool IsOnline;
+
+        public string Headshot;
+        public string Avatar;
+        public string Bust;
+
+        private List<ulong> Groups = new List<ulong>();
+
+        public RobloxUser(ulong UserId)
+        {
+            webClient = new WebClient();
+            string RAW_JSON;
+            try
+            {
+                RAW_JSON = webClient.DownloadString($"http://api.roblox.com/users/{UserId}");
+            }
+            catch
+            {
+                RAW_JSON = @"Bad Request";
+            }
+            if (RAW_JSON == "Bad Request")
+            {
+                Username = "Not Found";
+                Id = 0;
+                IsOnline = false;
+                return;
+            }
+            dynamic JSON = JObject.Parse(RAW_JSON);
+
+            Username = JSON.Username;
+            Id = JSON.Id;
+            IsOnline = JSON.IsOnline;
+
+            Headshot = $"https://www.roblox.com/headshot-thumbnail/image?userId={Id}&width=420&height=420&format=png";
+            Avatar = $"https://www.roblox.com/avatar-thumbnail/image?userId={Id}&width=420&height=420&format=png";
+            Bust = $"https://www.roblox.com/bust-thumbnail/image?userId={Id}&width=420&height=420&format=png%20true";
+        }
+        public RobloxUser(string username)
+        {
+            webClient = new WebClient();
+            string RAW_JSON;
+            try
+            {
+                RAW_JSON = webClient.DownloadString($"http://api.roblox.com/users/get-by-username?username={username}");
+            }
+            catch
+            {
+                RAW_JSON = @"User not found";
+            }
+            if (RAW_JSON.Contains("User not found"))
+            {
+                Username = "Not Found";
+                Id = 0;
+                IsOnline = false;
+                return;
+            }
+            dynamic JSON = JObject.Parse(RAW_JSON);
+            
+            Username = JSON.Username;
+            Id = JSON.Id;
+            IsOnline = JSON.IsOnline;
+
+            Headshot = $"https://www.roblox.com/headshot-thumbnail/image?userId={Id}&width=420&height=420&format=png";
+            Avatar = $"https://www.roblox.com/avatar-thumbnail/image?userId={Id}&width=420&height=420&format=png";
+            Bust = $"https://www.roblox.com/bust-thumbnail/image?userId={Id}&width=420&height=420&format=png%20true";
+        }
+
+        public List<ulong> getUserGroups(ulong UserID)
+        {
+            webClient = new WebClient();
+            string RAW_JSON;
+            try
+            {
+                RAW_JSON = webClient.DownloadString($"http://api.roblox.com/users/{UserID}/groups");
+            }
+            catch
+            {
+                RAW_JSON = @"NotFound";
+            }
+            if (RAW_JSON.Contains("NotFound"))
+            {
+
+            }
+            dynamic JSON = JsonConvert.DeserializeObject(RAW_JSON);
+            
+            foreach(var Group in JSON)
+            {
+                ulong groupId = Group.Id;
+                Groups.Add(groupId);
+            }
+
+            return Groups;
+        }
+        public void Dispose()
+        {
+            webClient.Dispose();
+        }
+    }
+}
